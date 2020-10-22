fb8c3ed8317eb2d3c34a48a8697c4f81181d7f94
Daniel Garcia <danigm87@correo.ugr.es>
2020-10-23 00:08:49 +0200

    Added RNCamera library and implemented example y settings tab

--- RNApp/src/view/screens/Settings.tsx
+++ RNApp/src/view/screens/Settings.tsx
@@ -1,12 +1,68 @@
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { SafeAreaView, StyleSheet, TouchableOpacity, Image, View, Text } from 'react-native'
 import { TYPOGRAPHY } from '../styles/typography'
-import React from 'react'
+import React, { useRef } from 'react'
 import { CText } from '../elements/custom'
+import { Navigation } from 'react-native-navigation'
+import { RNCamera } from 'react-native-camera'
 
 const Settings: React.FC = () => {
+  const showBurgerMenu = () => {
+    Navigation.mergeOptions('drawerComponentId', {
+      sideMenu: {
+        left: {
+          visible: true,
+        },
+      },
+    });
+  }
+
+  async function takePicture(){
+    if (this.cameraRef) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.cameraRef.takePictureAsync(options);
+      console.log(data.uri)
+      //TODO: GO TO OTHER SCREEN 
+      //SHOW THE IMAGE 
+      //SEND TO API 
+      //SPINNER WHILE API IS PROCESSING 
+      //SHOW RESULT
+    }
+  }
+
   return (
-    <SafeAreaView>
-      <CText>Settings</CText>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => showBurgerMenu()}>
+        <Image
+          style={styles.image}
+          resizeMode="contain"
+          source={require('../assets/images/burger-menu.png')}
+        />
+      </TouchableOpacity>
+      <View style={styles.container}>
+        <RNCamera
+          ref={(ref) => this.cameraRef = ref}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.off}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={takePicture} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> {'SNAP'} </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
@@ -14,9 +70,33 @@ const Settings: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex',
-    backgroundColor: TYPOGRAPHY.COLOR.Default,
+    flexDirection: 'column',
+    backgroundColor: 'white',
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+  image: {
+    marginBottom: 20,
+    width: 40,
+    height: 40,
+  },
+  button: {
+    alignSelf: 'center',
+    marginTop: 50,
+    width: 250,
+  }
 })
 
 export default Settings
