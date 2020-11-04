1fb4b4af16aafad84e0315e6ac0dc36b2b01d3c5
Antonio Molner <antonio.molner@strivelabs.io>
2020-11-04 21:03:09 +0000

    Add support for inference on GPU

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,17 @@ from io import BytesIO
 from pathlib import Path
 from typing import List
 import numpy as np
+from mxnet import npx
 from backend.modelling.face_identification import load_embeddings, inference, FaceAnalysis, Result
 
 app = FastAPI()
 drunk_embeddings, no_drunk_embeddings = load_embeddings(Path('../dataset'))
 face_analysis = FaceAnalysis()
-face_analysis.prepare(-1)
+
+if npx.num_gpus() > 0:
+    face_analysis.prepare(0)
+else:
+    face_analysis.prepare(-1)
 
 
 @app.post("/prediction/", response_model=List[Result])
